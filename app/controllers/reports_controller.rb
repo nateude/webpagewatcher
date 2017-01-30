6809e93ddfccce75b335dd3870849b5c806f3d22
Nate Ude <nate.ude@iloan.com>
2017-01-30 16:36:17 -0600

    added data call and table

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -13,6 +13,24 @@ class ReportsController < ApplicationController
     @report = Report.find(params[:id])
   end
 
+  def update
+    @report = Report.find(params[:id])
+    wpt = wpt_check_status(@report.wpt_id)
+    @report.update_attribute(:status, wpt)
+    if(wpt = "Test Complete")
+      wpt_data = wpt_get_data(@report.wpt_id)
+      @report.update_attribute(:data, wpt_data)
+    end
+    flash[:success] = "Report Updated"
+    redirect_to reports_path
+  end
+
+  def destroy
+    Report.find(params[:id]).destroy
+    flash[:success] = "Report deleted"
+    redirect_to reports_path
+  end
+
   def create
 
     new_report = report_params
--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,4 +1,14 @@
 module ReportsHelper
+
+  def valid_json?(json)
+    begin
+      JSON.parse(json)
+      return true
+    rescue JSON::ParserError => e
+      return false
+    end
+  end
+
   def wpt_url
     "http://www.webpagetest.org/results.php"
   end
@@ -34,5 +44,28 @@ module ReportsHelper
         :status => wpt['statusText'],
         :wpt_id => wpt['data']['testId']
       }
+  end
+
+  def wpt_check_status(report_id)
+      require 'net/http'
+      require 'json'
+      url = 'http://www.webpagetest.org/testStatus.php?f=json&test='+report_id
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+      wpt = JSON.parse(response)
+      results = wpt['statusText']
+  end
+  def wpt_get_data(report_id)
+      require 'net/http'
+      require 'json'
+      url = 'http://www.webpagetest.org/results.php?f=json&test='+report_id
+      uri = URI(url)
+      response = Net::HTTP.get(uri)
+  end
+  def parse_data(json_data)
+    if valid_json?(json_data)
+    else
+      'invalid JSON'
+    end
   end
 end
