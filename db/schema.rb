41351f5b7988e5c3fd6922a3a7f48a8bb6e5fc94
Nate Ude <nate.ude@iloan.com>
2017-02-09 09:29:43 -0600

    migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170208191008) do
 
   create_table "profiles", force: :cascade do |t|
