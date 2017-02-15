0e8cdb45131d6b58a6db308d36ffae31b4418916
Nate Ude <nate.ude@iloan.com>
2017-02-15 11:46:45 -0600

    added tests for data model and made them green

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,4 +1,7 @@
 class Website < ApplicationRecord
   has_many :profile
   has_many :report
+
+  validates :name, presence: true, uniqueness: true
+  validates :url, presence: true, uniqueness: true
 end
--- spec/website_spec.rb
+++ spec/website_spec.rb
@@ -3,28 +3,28 @@ require 'rails_helper'
 describe Website do
 
   it "is valid with name and url" do
-    website = Website.new(name: 'sample', url: 'http://www.sample.com')
+    website = Website.new(name: 'sample', url: 'http:://www.sample.com')
     expect(website).to be_valid
   end
 
   it "is invalid without a name" do
     website = Website.new(name: nil)
     website.valid?
-    expect(website.errors[:name]).to include("cant't be blank")
+    expect(website.errors[:name]).to include("can't be blank")
   end
   it "is invalid without a url" do
     website = Website.new(url: nil)
     website.valid?
-    expect(website.errors[:url]).to include("cant't be blank")
+    expect(website.errors[:url]).to include("can't be blank")
   end
   it "is invalid with a duplicate name" do
-    Website.new(name: 'sample', url: 'http:://www.sample.com')
+    Website.create(name: 'sample', url: 'http:://www.sample.com')
     website = Website.new(name: 'sample', url: 'http:://www.sample_new.com')
     website.valid?
     expect(website.errors[:name]).to include("has already been taken")
   end
   it "is invalid with a duplicate url" do
-    Website.new(name: 'sample', url: 'http:://www.sample.com')
+    Website.create(name: 'sample', url: 'http:://www.sample.com')
     website = Website.new(name: 'sample_new', url: 'http:://www.sample.com')
     website.valid?
     expect(website.errors[:url]).to include("has already been taken")
