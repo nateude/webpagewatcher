f8fffab1d353104b006bcc26a60b8ea94f2eeb9f
Nate Ude <nate.ude@iloan.com>
2017-02-16 08:54:31 -0600

    add data model specs for report

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,10 @@
 class Report < ApplicationRecord
   belongs_to :profile
   belongs_to :website
+
+  validates :website_id, presence: true
+  validates :profile_id, presence: true
+  validates :wpt_id, presence: true
+  validates :status, presence: true
+  validates :status_code, presence: true
 end
