c6fdb20fd7189cde6e50f1002054a489a50ed6a6
Nate Ude <nate.ude@iloan.com>
2017-04-05 10:53:53 -0500

    converted from describe/it to feature/scenario cleanded up formating and language

--- spec/features/websites_spec.rb
+++ spec/features/websites_spec.rb
@@ -1,90 +1,76 @@
 require 'rails_helper'
 
-feature 'Websites' do
-  before :each do
+feature 'Websites homepage' do
+  before do
     generate_sample_data
+    visit websites_path
   end
-
-  describe 'index' do
-    before :each do
-      visit websites_path
-    end
-    it 'has accessible page' do
-      page_should_exist
-    end
-    it 'has website data' do
-      should_see '1 Example Website http://www.examplewebsite.com/'
-    end
-    it 'can navigate to single website' do
-      click_on 'view', match: :first
-      page_should_exist
-    end
+  scenario 'has accessible page' do
+    page_should_exist
   end
-
-  describe 'subpage' do
-    before :each do
-      visit website_path(1)
-    end
-    it 'has website settings' do
-      should_see 'ID 1'
-      should_see 'Name Example Website'
-      should_see 'URL http://www.examplewebsite.com/'
-    end
-    it 'has profiles' do
-      should_see 'EW - Homepage'
-      should_see 'EW - Subpage'
-    end
-    it 'can navigate to profile' do
-      click_on 'view', match: :first
-      page_should_be(profile_path(1))
-    end
-    it 'has reports' do
-      should_see 'EW - Homepage 12345'
-      should_see 'EW - Homepage 23456'
-    end
-    it 'can navigate to report' do
-      # TODO: I don't like this syntax, find a better way
-      within_table_row(8) do
-        click_on 'view'
-      end
-      page_should_be(report_path(1))
-    end
+  scenario 'has website data' do
+    should_see '1 Example Website http://www.examplewebsite.com/'
+  end
+  scenario 'can navigate to single website' do
+    click_on 'view', match: :first
+    page_should_exist
   end
 end
 
-feature 'New Website' do
-    before :each do
-      visit new_website_path
-    end
-
-  describe 'Page' do
-    it 'has a header' do
-      should_see 'Add a New Website'
-    end
-
-    it 'has a submittable form' do
-      fill_in 'Name', with: 'Example Website'
-      fill_in 'Url', with: 'http://www.examplewebsite.com'
-      click_on 'Add New Site'
-      should_see 'Example Website http://www.examplewebsite.com'
-    end
-
-    it 'has errors empty name' do
-      fill_in 'Name', with: nil
-      fill_in 'Url', with: 'http://www.examplewebsite.com'
-      click_on 'Add New Site'
-      should_see "Name can't be blank"
-    end
-
-    it 'has errors empty url' do
-      fill_in 'Name', with: 'Example Website'
-      fill_in 'Url', with: nil
-      click_on 'Add New Site'
-      should_see "Url can't be blank"
-    end
+feature 'Website subpage' do
+  before do
+    generate_sample_data
+    visit website_path(1)
   end
-
-  describe 'form' do
+  scenario 'has website settings' do
+    should_see 'ID 1'
+    should_see 'Name Example Website'
+    should_see 'URL http://www.examplewebsite.com/'
   end
+  scenario 'has profiles' do
+    should_see 'EW - Homepage'
+    should_see 'EW - Subpage'
+  end
+  scenario 'can navigate to profile' do
+    click_on 'view', match: :first
+    page_should_be(profile_path(1))
+  end
+  scenario 'has reports' do
+    should_see 'EW - Homepage 12345'
+    should_see 'EW - Homepage 23456'
+  end
+  scenario 'can navigate to report' do
+    # TODO: I don't like this syntax, find a better way
+    within_table_row(8) do
+      click_on 'view'
+    end
+    page_should_be(report_path(1))
+  end
+end
 
+feature 'New webiste' do
+  before do
+    visit new_website_path
+  end
+  scenario 'has a header' do
+    should_see 'Add a New Website'
+  end
+  scenario 'has a submittable form' do
+    fill_in 'Name', with: 'Example Website'
+    fill_in 'Url', with: 'http://www.examplewebsite.com'
+    click_on 'Add New Site'
+    should_see 'Example Website http://www.examplewebsite.com'
+  end
+  scenario 'form has errors when name is empty' do
+    fill_in 'Name', with: nil
+    fill_in 'Url', with: 'http://www.examplewebsite.com'
+    click_on 'Add New Site'
+    should_see "Name can't be blank"
+  end
+  scenario 'form has errors when url is empty' do
+    fill_in 'Name', with: 'Example Website'
+    fill_in 'Url', with: nil
+    click_on 'Add New Site'
+    should_see "Url can't be blank"
+  end
 end
