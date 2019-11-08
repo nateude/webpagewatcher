99ccb068d6e591c9e56ebf77bf256147a7f7b7e3
Nate <nateude@gmail.com>
2019-11-08 12:48:02 -0700

    rubocop fix

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -2,6 +2,7 @@ module ProfilesHelper
   def generate_report(profile)
     { report: { website_id: profile.website_id, profile_id: profile.id } }
   end
+
   def pending_reports(profile)
     profile.report.pending.empty?
   end
