d75328b392d245be5047439560b6f8e7d2497e50
Nate <nateude@gmail.com>
2019-12-16 12:01:45 -0700

    layout clean up & authenticated views

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,4 @@
-class ReportsController < ApplicationController
+class ReportsController < AuthenticatedController
   include ApplicationHelper
   include ReportsHelper
 
--- app/controllers/webpagetest_api_controller.rb
+++ app/controllers/webpagetest_api_controller.rb
@@ -1,4 +1,4 @@
-class WebpagetestApiController < ApplicationController
+class WebpagetestApiController < AuthenticatedController
   def init
     @test = InitTest.new(profile: Profile.find(params[:id])).run
   end
